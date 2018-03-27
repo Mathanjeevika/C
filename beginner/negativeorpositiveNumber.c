83ad40e3c7103b42ec9756d53531cc9da847ea98
Mathanjeevika <Mathanjeevika@users.noreply.github.com>
2018-03-27 14:48:50 +0530

    Create negativeorpositiveNumber.c

---
+++ beginner/negativeorpositiveNumber.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	long int i=200000;
+	if(i>1 && i<100000){
+		printf("The Given Value is Positive");
+	}
+	else if(i==0){
+		printf("The Number is Neither Negative nor Positive");
+	}
+	else if(i>100000){
+		printf("The Number is Greater than 100000");
+	}
+	else{
+		printf("The Number Is Negative");
+	}
+	return 0;
+}
